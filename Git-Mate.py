46d6caf41dc3d2a5990a0242bbe162c859d3a123
Rashen Anupama <mn4raisan@gmail.com>
2025-05-28 11:36:00 +0530

    Sketch the code structure

---
+++ Git-Mate.py
@@ -0,0 +1,28 @@
+from tkinter import *
+root = Tk()
+from subprocess import *
+
+# PATH - input
+
+
+# RUN - CMD
+
+
+# ADD Date and Time
+
+
+# RUN - Date / Time
+
+
+# Count - input commit count
+
+
+# RUN - CMD HEAD
+
+
+# UPDATE Total count + Refresh GUI
+
+
+# Additionals
+# 1. Commit History
+# 2. Branch Check
