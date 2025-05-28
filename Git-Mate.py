075dc7c03e9aa375a98c1b844c1255810f152803
Rashen Anupama <mn4raisan@gmail.com>
2025-05-28 11:40:11 +0530

    update structure and add CMD codes for git

--- Git-Mate.py
+++ Git-Mate.py
@@ -6,23 +6,25 @@ from subprocess import *
 
 
 # RUN - CMD
-
-
-# ADD Date and Time
-
-
-# RUN - Date / Time
-
+# > cd "C:\Users\4Raisan\Desktop\GitHub\Git-Mate"
 
 # Count - input commit count
 
 
 # RUN - CMD HEAD
-
+# > git reset --hard HEAD~1
 
 # UPDATE Total count + Refresh GUI
 
 
+# Done - Confirm
+# > git push origin main --force
+
+
 # Additionals
+
 # 1. Commit History
+# > git log --oneline
+
 # 2. Branch Check
+# > git status
