a9feadf8f9a369eaf4370814682adb494c141294
Rashen Anupama <mn4raisan@gmail.com>
2025-06-02 14:53:41 +0530

    Update the grid structure for more clearence

--- Git-Mate.py
+++ Git-Mate.py
@@ -11,29 +11,35 @@ root.resizable(False, False)
 
 
 # Tops + Additionals + Guide + Counter
+justspace1 = Label(root, text="-")
+justspace1.grid(row=0, column=0)
+
 name = Button(root, text="Git-Mate")
-name.grid(columnspan=2, row=0, column=0, ipadx=10, ipady=3, pady=10)
+name.grid(columnspan=2, row=0, column=1, ipadx=10, ipady=3, pady=10,padx=3)
 
 guide = Button(root, text="User Guide")
-guide.grid(row=0, column=2, ipadx=10, ipady=3, pady=10)
+guide.grid(columnspan=2, row=0, column=3, ipadx=10, ipady=3, pady=10,padx=3)
 
 adt = Button(root, text="additionals")
-adt.grid(row=0, column=3, ipadx=10, ipady=3, pady=10)
+adt.grid(columnspan=2, row=0, column=5, ipadx=10, ipady=3, pady=10,padx=3)
 
 refreshCount = 0
-scnt = Label(root, text=f"Undos: {refreshCount}")
-scnt.grid(row=0, column=4, ipadx=10, ipady=3, pady=10)
+scnt = Label(root, text=f"  Undos: {refreshCount}")
+scnt.grid(columnspan=2, row=0, column=7, ipadx=10, ipady=3, pady=10)
+
+justspace2 = Label(root, text="-")
+justspace2.grid(row=0, column=10)
 
 
 # PATH ---------------------------------------------------S
 
 #---------Display
 path = Label(root, text="PATH : ")
-path.grid(row=1 ,column=1)
+#path.grid(row=1 ,column=1)
 
 epath = Entry(root, fg="grey")
 epath.insert(0,"Enter REPOs local path...")
-epath.grid(columnspan=3, row=1, column=2, ipadx=75, ipady=3, pady=5)
+#epath.grid(columnspan=3, row=1, column=2, ipadx=75, ipady=3, pady=5)
 
 #---------Focus
 def focusepath(event):
@@ -61,11 +67,11 @@ def checkpath():
 
 # Count - input commit count
 count = Label(root, text="UNDOs : ")
-count.grid(row=2, column=1)
+#count.grid(row=2, column=1)
 
 ecount = Entry()
 ecount.insert(0,"1")
-ecount.grid(row=2, column=2,ipady=3, pady=5)
+#ecount.grid(row=2, column=2,ipady=3, pady=5)
 
 
 # RUN - CMD HEAD
@@ -84,11 +90,11 @@ def totcount(new):
 # Done - Confirm
 # > git push origin main --force
 Ctype = Label(root, text="Enter 'C' for confirm\nRemember their is no REDOs")
-Ctype.grid(rowspan=2, columnspan=3, row=3, column=0)
+#Ctype.grid(rowspan=2, columnspan=3, row=3, column=0)
 
 econfirm = Entry()
 econfirm.insert(0,"")
-econfirm.grid(columnspan=3, row=5, column=0)
+#econfirm.grid(columnspan=3, row=5, column=0)
 
 def confirmproceed():
     if (econfirm.get()).upper() == "C":
@@ -97,7 +103,7 @@ def confirmproceed():
 
 
 CButton = Button(root, text="UNDO", command=lambda: (confirmproceed(),checkpath()))
-CButton.grid(columnspan=3, row=6, column=0)
+#CButton.grid(columnspan=3, row=6, column=0)
 
 
 # Additionals
