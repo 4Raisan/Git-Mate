aec41fc310b56998b607faa71a1d1643edd6845c
Rashen Anupama <mn4raisan@gmail.com>
2025-06-07 10:26:49 +0530

    fix check boc alignment

--- Git-Mate.py
+++ Git-Mate.py
@@ -133,7 +133,7 @@ direpath = False
 # Variable to track checkbox state (1 = checked, 0 = unchecked) 
 check_vrbl = IntVar()
 checkbox =  Checkbutton(root, text="Confirm and Check Details", variable=check_vrbl, command=lambda: (check_conf(), passcheck()))
-checkbox.grid(row=6, column=3)
+checkbox.grid(columnspan=4, row=6, column=3)
 
 #--------------------------------------------
 
