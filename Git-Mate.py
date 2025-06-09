82b5e80bb2d4c370b0398a7dcc4e5a09f824ebb0
Rashen Anupama <mn4raisan@gmail.com>
2025-06-10 02:22:47 +0530

    Row 3/4/5 main programming done

--- Git-Mate.py
+++ Git-Mate.py
@@ -131,12 +131,17 @@ epathclear.grid(row=2, column=8)
 # DONE
 
 
+# DONE
 # Instruction 3/4/5-Row---------------------------------------------------
 
 instruction_avoid = Label(root, text="\nAvoid use, if your last commits have File Uploads !", fg='red')
 instruction_avoid.grid(rowspan=2, columnspan=8, row=3, column=2)
 
+instruction_redo = Label(root, text="Remember their is no REDOs")
+instruction_redo.grid(columnspan=8, row=5, column=1)
+
 #Z Instruction 3/4/5-Row---------------------------------------------------
+# DONE
 
 
 # Check box 6-Row------------------------------------------------------
